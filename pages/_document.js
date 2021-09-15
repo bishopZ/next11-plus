c2f7f293435a63b20da13f3712262a848cd70e06
Bishop Zareh <xchicago@gmail.com>
2021-09-15 14:26:19 -0600

    update tsconfig

--- pages/_document.js
+++ pages/_document.js
@@ -1,7 +1,7 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
-  render() {
+  static render() {
     return (
       <Html lang="en-us">
         <Head>
@@ -15,8 +15,8 @@ class MyDocument extends Document {
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
 
-export default MyDocument
+export default MyDocument;

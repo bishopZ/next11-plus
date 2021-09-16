d111c756a76d8f48fa80ac218b81cfd1cca63bc3
Bishop Zareh <xchicago@gmail.com>
2021-09-15 18:05:05 -0600

    update dependency versions

--- components/atoms/articleAuthor.tsx
+++ components/atoms/articleAuthor.tsx
@@ -2,12 +2,12 @@ import styles from '../../styles/modules/Typography.module.scss';
 
 interface Props { author?: { name?: string; } }
 
-const minLength = 0;
+const minLength = 1;
 
 /** generic display of an author's name */
 const ArticleAuthor = ({ author }: Props) => (
   <>
-    { author && author.name && author.name.length > minLength &&
+    { author && author.name && author.name.length >= minLength &&
       <p className={styles.author}>{`by ${author.name}`}</p>
     }
   </>
